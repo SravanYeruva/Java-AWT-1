1c68b53ddc205061949af5537cce2a8ac2fd5445
Sravan Yeruva <55128679+SravanYeruvah@users.noreply.github.com>
2019-12-12 16:06:21 +0530

    code.java

---
+++ code.java
@@ -0,0 +1,20 @@
+import java.awt.*;  
+class First extends Frame
+{  
+First()
+{  //coded by sravan yeruva
+Button b=new Button("click me");  
+b.setBounds(100,100,100,100);
+//coded by sravan yeruva
+add(b);
+setSize(300,300);//coded by sravan yeruva
+setLayout(null);
+setVisible(true); 
+}  
+//coded by sravan yeruva
+public static void main(String args[])
+{  
+//coded by sravan yeruva
+First f=new First();  
+}
+}  
